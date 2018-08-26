b170f74b80f09e31b7c074046dc1d1d002fe2253
Hefei Li <lhfeilaile@gmail.com>
2018-08-26 10:27:09 +0800

    init import

---
+++ src/main/java/cn/lhfei/flink/service/impl/WordCountImpl.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2010-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.lhfei.flink.service.impl;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.util.Collector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import cn.lhfei.flink.service.WordCountService;
+import cn.lhfei.flink.service.support.WordCountData;
+
+/**
+ * @version 0.1
+ *
+ * @author Hefei Li
+ *
+ * @Created 8月 26, 2018
+ */
+@Service("wordCountService")
+public class WordCountImpl implements WordCountService {
+	private static final Logger LOG = LoggerFactory.getLogger(WordCountImpl.class);
+
+	public DataSet<Tuple2<String, Integer>> count(String[] args) throws Exception {
+		
+		final ParameterTool params = ParameterTool.fromArgs(args);
+
+		// set up the execution environment
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+		// make parameters available in the web interface
+		env.getConfig().setGlobalJobParameters(params);
+
+		// get input data
+		DataSet<String> text;
+		if (params.has("input")) {
+			// read the text file from given input path
+			text = env.readTextFile(params.get("input"));
+		} else {
+			// get default test text data
+			LOG.debug("Executing WordCount example with default input data set.");
+			LOG.debug("Use --input to specify file input.");
+			text = WordCountData.getDefaultTextLineDataSet(env);
+		}
+
+		DataSet<Tuple2<String, Integer>> counts =
+				// split up the lines in pairs (2-tuples) containing: (word,1)
+				text.flatMap(new Tokenizer())
+				// group by the tuple field "0" and sum up tuple field "1"
+				.groupBy(0)
+				.sum(1);
+
+		return counts;
+		
+	}
+	
+	// *************************************************************************
+	// USER FUNCTIONS
+	// *************************************************************************
+
+	/**
+	 * Implements the string tokenizer that splits sentences into words as a
+	 * user-defined FlatMapFunction. The function takes a line (String) and splits
+	 * it into multiple pairs in the form of "(word,1)"
+	 * ({@code Tuple2<String, Integer>}).
+	 */
+	public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+		@Override
+		public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+			// normalize and split the line
+			String[] tokens = value.toLowerCase().split("\\W+");
+
+			// emit the pairs
+			for (String token : tokens) {
+				if (token.length() > 0) {
+					out.collect(new Tuple2<>(token, 1));
+				}
+			}
+		}
+	}
+}
